520df1d8f2efea7292330be1e121a5569e142833
Dmytro Popovskyi <popovskiy96@gmail.com>
2026-06-15 12:23:15 +0200

    setup connection to database and apply migrations

--- hw4/settings.py
+++ hw4/settings.py
@@ -3,12 +3,23 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-sa*c@!96k_5*i*j)%iyamxmy87$vf-%bge1+@xpmr4c(p#swxj"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
-INSTALLED_APPS = []
+INSTALLED_APPS = ["hello.apps.HelloConfig"]
 
 ROOT_URLCONF = "hw4.urls"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
