c2bd0766f472b82f12528d749095765c4e0e21dd
Dmytro Popovskyi <popovskiy96@gmail.com>
2026-07-11 10:19:54 +0200

    fix argocd output

--- modules/argo_cd/outputs.tf
+++ modules/argo_cd/outputs.tf
@@ -8,7 +8,7 @@ data "kubernetes_service" "argocd" {
 }
 
 output "argocd_url" {
-  value = "https://${data.kubernetes_service.argocd.status[0].load_balancer[0].ingress[0].hostname}"
+  value = "http://${data.kubernetes_service.argocd.status[0].load_balancer[0].ingress[0].hostname}"
 }
 
 output "argocd_namespace" {
